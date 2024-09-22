5b215851b5f7aa03b177271a2e3b1bf8a53ef356
sonnv <sonnv1912@gmail.com>
2024-09-22 22:40:17 +0700

    feat: add update button in update log screen

--- screens/update-log.tsx
+++ screens/update-log.tsx
@@ -1,27 +1,102 @@
+import { Button } from '@/components/form/button';
+import { LoadingOverlay } from '@/components/layout/loading-overlay';
 import { Screen } from '@/components/layout/screen';
 import { ScreenHeader } from '@/components/layout/screen-header';
+import { app$ } from '@/stores';
 import type { ScreenProps } from '@/types';
+import { observer, useObservable } from '@legendapp/state/react';
 import { ScrollView, Text, View } from 'dripsy';
+import { checkForUpdateAsync, fetchUpdateAsync } from 'expo-updates';
 import { Fragment } from 'react/jsx-runtime';
 
-const UpdateLogScreen = ({ route }: ScreenProps<'UpdateLogScreen'>) => {
-	const { updates } = route.params;
+const UpdateLogScreen = observer(
+	({ route, navigation }: ScreenProps<'UpdateLogScreen'>) => {
+		const { updates } = route.params;
+		const update$ = useObservable<string[]>(updates);
+		const loading$ = useObservable<boolean>(false);
 
-	return (
-		<Screen>
-			<ScreenHeader />
+		const onUpdate = async () => {
+			update$.set([]);
+			loading$.set(true);
 
-			<ScrollView contentContainerSx={{ padding: 'md', paddingBottom: 50 }}>
-				{updates.map((t) => (
-					<Fragment key={t}>
-						<Text>{t}</Text>
+			try {
+				update$.push('Checking for updates...');
 
-						<View sx={{ height: 2, backgroundColor: 'gray400', my: 'lg' }} />
-					</Fragment>
-				))}
-			</ScrollView>
-		</Screen>
-	);
-};
+				const upd = await checkForUpdateAsync();
+
+				update$.push(
+					`update Available? ${upd.isAvailable} - ${JSON.stringify(upd, null, 2)}`,
+				);
+
+				if (upd.isAvailable) {
+					update$.push('About to Fetch... ');
+
+					const fetched = await fetchUpdateAsync();
+
+					update$.push(
+						`Update fetched? ${fetched.isNew} - ${JSON.stringify(fetched, null, 2)}`,
+					);
+				}
+
+				navigation.navigate('UpdateLogScreen', {
+					updates: update$.get(),
+				});
+
+				loading$.set(false);
+				update$.set([]);
+			} catch (e) {
+				update$.push(`Error: ${JSON.stringify(e, null, 2)}`);
+
+				navigation.navigate('UpdateLogScreen', {
+					updates: update$.get(),
+				});
+
+				loading$.set(false);
+				update$.set([]);
+			}
+		};
+
+		return (
+			<Screen>
+				{loading$.get() && <LoadingOverlay text='Getting your update' />}
+
+				<ScreenHeader />
+
+				<ScrollView contentContainerSx={{ padding: 'md', paddingBottom: 50 }}>
+					{update$.get().map((t) => (
+						<Fragment key={t}>
+							<Text>{t}</Text>
+
+							<View sx={{ height: 2, backgroundColor: 'gray400', my: 'lg' }} />
+						</Fragment>
+					))}
+				</ScrollView>
+
+				<View
+					sx={{
+						position: 'absolute',
+						bottom: 12,
+						right: 12,
+						zIndex: 999,
+						gap: 'md',
+						p: 'md',
+						backgroundColor: 'primary500',
+						borderRadius: 'full',
+					}}
+				>
+					<Button
+						rightIcon='FileCloudIcon'
+						schema='black'
+						iconSx={{
+							width: app$.font.get(),
+							height: app$.font.get(),
+						}}
+						onPress={onUpdate}
+					/>
+				</View>
+			</Screen>
+		);
+	},
+);
 
 export { UpdateLogScreen };
